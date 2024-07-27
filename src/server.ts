4ef6b2219ba43cd1ae1845518b58b3616e52f579
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2024-07-27 16:50:13 -0300

    chore: removendo endpoints de teste

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import Express from 'express';
 import cors from 'cors';
-import mainRoute from "./routes"
 import contractRoutes from './routes/contractRoutes';
 
 const app = Express();
@@ -11,11 +10,10 @@ app.use(Express.json());
 
 app.use(cors());
 
-app.use("/", mainRoute)
-
 app.use("/contract", contractRoutes)
 
 const server = app.listen(PORT, () =>{
   console.log(`Server running on ${PORT}`)
 })
 
+export { server };
