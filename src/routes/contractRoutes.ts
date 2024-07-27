21ed143c4397392caf59de8599a579be065415f4
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2024-07-27 16:52:03 -0300

    feat: rotas para edição, listagem e remoção
    Refs: #2

--- src/routes/contractRoutes.ts
+++ src/routes/contractRoutes.ts
@@ -3,7 +3,10 @@ import contractController from "../controllers/contractController";
 
 const contractRoutes = Router();
 
-contractRoutes.post("/", contractController.create );
+contractRoutes.get("/", contractController.list);
+contractRoutes.post("/", contractController.create);
+contractRoutes.put("/:id", contractController.edit);
+contractRoutes.patch("/:id", contractController.toggleContract);
 
 export default contractRoutes;
 
