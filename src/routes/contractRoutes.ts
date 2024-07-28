62a7cc23c6d05a9a449772afd12c9c39849967b3
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2024-07-27 21:38:48 -0300

    adicionando logger para debug

--- src/routes/contractRoutes.ts
+++ src/routes/contractRoutes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import contractController from "../controllers/contractController";
+import logger from "../middleware/logger";
 
 const contractRoutes = Router();
+
+// Aplicar o middleware de log a todas as rotas de contrato
+contractRoutes.use(logger);
 
 contractRoutes.get("/", contractController.list);
 contractRoutes.post("/", contractController.create);
